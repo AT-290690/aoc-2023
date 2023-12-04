525565102202db149ea8e33d380189375853afb3
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-04 16:34:59 +0200

    Fix lisp solution day 4 part 1 AT

--- 4/AT-lisp/main.lisp
+++ 4/AT-lisp/main.lisp
@@ -1,5 +1,5 @@
 ; To run this code go follow this link
-; https://at-290690.github.io/fez/index.html?l=NwAgCgTgpgzjIGMC2ATEBqE8D2FEBsBLBAaxGwDsQAXACykVsIAdy86BXClKPYy%2BACgAFPijUQzAIYQYDUVKQAjFFJCEKzDhOHNC6zdpDCY1CBoDmALhjMiEgEQAdCg4CUxmRCkBPK0ilWBWVVEAAPYxRsEEEQYzEJL2NTcwprW3twkAcrbLc3WPjxAx4I3X1CuKrqmqTKmoa44QQZD3rGmpMzSxs7Qkc8mI6O5pQINuHG5tahyc6W0xtutIBaAD4KDmVefIKmhMQZFHhyuPbhurmq0fHZq%2Bnb85GUnoz%2B7IAfdzu54S9ffyBeKKFRqCJPK6%2FPQ%2FSGwrIQuHDLqpdJ9AbuBGIqb%2FPxyMQIHT4EGhCJhfKYrHzKSLF5pGDrTbbWS7FmskQHGCKOzyaSyBgOADCRxAAEZcgAWEUgcUADhAMoAzPKAGyigDsIA%2B8qVMtVIFVCqlIo1IAAnNK5QBWBWCIUQNAAJlyIqVCodIAdAAZRarlVKtX6QArvcq5TL3cag%2B6HeLRabbcKFblRR6pdaQJbzeLYwHzeH9Uq1RHVQ6jbHRQn7dKJVLTe61TrY8rzVrM%2FLY2rVZa03KMxnY4rK2hLbkZRrFR6S3K3Zr5dPvWrvSL3aalQ73fmFcqh%2FrcobRXKXRnVUXZ2qOxqRd6Ha7LfqNVvRSL3AVRMVedQpcFQQYtBIKVCFSUg0Gh%2FjCwFNDiVgAGbYPgaDfqEahKJE0QAZSb4SGI0E6C0eBKOSEGTJhIDmBYtC4TIxgIGMIAEYRREjAcKAcChfwQN4fiwfBpGEORhLEmoyHJOIVhSCgKAAIQgMh%2BSeBxvjGB4wjKapSm7OhWIkQE1AIPQ8CensjFTAc0AAO7CiKRnGZ0HIILgsDyZxIBLgxNnVOxnEwbgIDYQJIRqHhaCae5UxmfQVAmKJtDUgA%2FCALEoUFylROBoWIp5AJyNQ0kwPZ0DwDpemOcImCZX4FjFHlDmFVIun6R4VnWelGFQdl0lqN6pXUVRUjKVBlUSNVBWkVAFn2hpLUQcI4VQJFaxOQCg1YPljlFQ1ooTFNLXlTY4i5at8DmcKIXbXCwgAFSLRVVWHaN40oB4DqsnJp2wn1zU2eVLmvaFDwsuy2AWNJugyJ%2BWBcmI%2BRAA%3D%3D
+; https://at-290690.github.io/fez/index.html?l=NwAgCgTgpgzjIGMC2ATEBqE8D2FEBsBLBAaxGwDsQAXACykVsIAdy86BXClKPYy%2BAChBACnxRqIZgEMIMBmOlIARimkhCFZh0kjmhDVp0gRMahE0BzAFwxmRSQCIAOhUcBKE7IjSAntaRpVkUVNRAADxMUbBBBEBNxSW8TMwsKGzsHCJBHaxz3dziEiUMeSL0DIvjqmtrkqtrG%2BJEEWU8GptrTcytbe0InfNjOzpaUCHaRppa24amu1rNbHvSAWgA%2BCg4VXgLC5sTEWRR4CviOkfr56rGJueuZu4vR1N7MgZyAHw97%2BZFvPwBIIJJSqdSRZ7XP76X5QuHZSHwkbdNIZfqDDyIpHTAH%2BeTiBC6fCgsKRcIFLHYhbSJavdIwDZbHZyPZ7Ir%2FCA%2BXylKCRVoQE6stliEowJT2BQyOQMRwAYWOIAAjHkACyKkAqgAcIE1AGYdQA2JUAdhAnx1%2Bs1RpARt16sVppAAE4NdqAKy6wTygUgABMeUV%2Bt1vr9AAYlUaDerzVGQLrwwbtZqQw64yHfSqlU6vQrdXklX71R6QG6XSrMzGXcmbfrjSmjb77ZmlTmfSrVeqnSHjZbMwaXebSzrM8ajW6i9qSyXM3rW2g3XlNaa9X6G9rg2adevw8bw4qQ079b6Q9XdQa5za8naldrAyWjXXN8aR6bFeHfUG3TbTWelYqPIUIqSFK1DqiEYKGNokiUtClRUo0mhQTBcIclyQLBMSoTgiY%2FI4eMESsrC8HNLi1gAGbYPgaDgWE6jKFEMTIdiQEgOIZG6LhygUsRUwsRYli0Bxsh4XgXHcTxoyHCgHD0ahgIUVRID8YJIJYSAdEpBI1jSCgKAAITqSAYleJyfgmJ4IgWVZ5lshJkklIE1AIPQ8ChvsdmNHxUAAO4Koq7keTULEwAguCwCZXIgHu4mBTcpEUXgbFEiS6j8mgTGxZ53n0FQphabQNIAPwgNJ9FpRZ0REZl8JyXiEgGSFYXwI5znhSImC1dYliiqF0DNdITkuZ4%2FkBdVzGkfI1AGeo4btThwnSBZpHdZIjV9UpPnHLZY1UiI2VQLl6wRYCK1YL14UtUNSqTDtmWdZNDXnfA0C%2BT6GW3ShABUx3%2BKda3hS9W1%2BkKBRVXZi2jR5tVRaD728fyrKiPg2CWAZeiyKBWDiuIBRAA
 
 (let parse (lambda input (pi input (string:split "\n") (array:map (lambda x (do 
   (let arr (string:split x ": "))
@@ -20,7 +20,8 @@
                                 x 
                                 (string:split " ")
                                 (array:select (lambda x x))
-                                (cast:strings->numbers))))))))))))
+                                (cast:strings->numbers))))))
+  (array index cards)))))))
 (let sample (parse "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
@@ -29,7 +30,8 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"))
 (let part1 (lambda input 
                       (pi 
-                        input 
+                        input
+                          (array:map (lambda x (car (cdr x)))) 
                           (array:fold (lambda a b (do 
                               (let left (car b))
                               (let right (car (cdr b)))
@@ -47,4 +49,5 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"))
                               a))
                             (array 0)) 
                           (car))))
+
 (log! (part1 sample))
