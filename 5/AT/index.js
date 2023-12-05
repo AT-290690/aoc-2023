daa5e5f15480a79bcdf3c6eb7a1181ef1bc047e0
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-06 01:02:21 +0200

    Add optimal solution for part 1

--- 5/AT/index.js
+++ 5/AT/index.js
@@ -2,32 +2,42 @@ const { readFileSync } = require('fs')
 const dir = __dirname.split('/')
 dir.pop()
 const parse = (input) => {
+  // // JavaScript object is not guaranteed to order...
+  // const order = {
+  //   'seed-to-soil': 0,
+  //   'soil-to-fertilizer': 1,
+  //   'fertilizer-to-water': 2,
+  //   'water-to-light': 3,
+  //   'light-to-temperature': 4,
+  //   'temperature-to-humidity': 5,
+  //   'humidity-to-location': 6,
+  // }
   const [lines, ...rest] = input
     .trim()
     .split('\n\n')
     .map((x) => x.trim().split('\n'))
   const [seeds] = lines
-  const maps = rest.reduce((a, [map, ...coords]) => {
-    a.set(
-      map.slice(0, -5),
-      coords.map((x) => x.split(' ').map(Number))
-    )
+  const maps = rest.reduce((a, [name, ...coords]) => {
+    a.push({
+      name: name.slice(0, -5),
+      map: coords.map((x) => x.split(' ').map(Number)),
+    })
     return a
-  }, new Map())
+  }, [])
+  // .sort((a, b) => (order[a.name] > order[b.name] ? 1 : -1))
   return {
     seeds: seeds
       .split('seeds: ')[1]
       .split(' ')
       .reduce((a, b) => {
-        a[b] = +b
+        a.push(+b)
         return a
-      }, {}),
+      }, []),
     maps,
   }
 }
 const input = parse(readFileSync(`${dir.join('/')}/AT/input.txt`, 'utf-8'))
-const sample = parse(`
-seeds: 79 14 55 13
+const sample = parse(`seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
@@ -66,44 +76,60 @@ seeds: 79 14 55 13
 seed-to-soil map:
 50 98 2
 52 50 48`)
-const toRange = (offset, length) =>
-  Array.from({ length })
-    .fill(null)
-    .map((_, i) => offset + i)
-const toBoundsEnd = (offset, length) => offset + length - 1
-const part1 = ({ seeds, maps }) => {
-  const correspondences = {}
-  for (const [key, source] of maps) {
-    correspondences[key] = []
-    for (const current of source) {
-      const [start, end] = current
-      const length = current.pop()
-      const obj = {}
-      correspondences[key].push(obj)
-      current[0] = toRange(start, length)
-      current[1] = toRange(end, length)
-      const [A, B] = current
-      for (let i = 0; i < A.length; ++i) obj[B[i]] = A[i]
-    }
-    correspondences[key] = correspondences[key].reduce(
-      (a, b) => ({ ...a, ...b }),
-      {}
-    )
-  }
-  const output = []
-  for (const seed in seeds) {
-    let left = seed
-    const obj = { seed: seeds[seed] }
-    for (const corr in correspondences) {
-      const right = correspondences[corr]
-      const current = right[left] ?? +left
-      obj[corr.split('-to-')[1]] = current
-      left = current
-    }
-    output.push(obj)
-  }
-  return Math.min(...output.map(({ location }) => location))
-}
 
+const part1 = ({ seeds, maps }) => {
+  const range = []
+  for (const seed of seeds) {
+    let temp = seed
+    for (const { name, map } of maps) {
+      for (const [start, end, length] of map) {
+        if (temp >= end && temp < end + length) {
+          temp = temp - end + start
+          break
+        }
+      }
+    }
+    range.push(temp)
+  }
+  return Math.min(...range)
+}
+const part2 = ({ seeds, maps }) => {
+  seeds = seeds.reduce((a, b, i) => {
+    if (i % 2 === 0) a.push([b])
+    else {
+      const prev = a.at(-1)
+      prev.push(prev[0] + b - 1)
+    }
+    return a
+  }, [])
+  let min = Infinity
+  for (const [start, end] of seeds) {
+    for (let seed = start; seed < end; ++seed) {
+      let temp = seed
+      let tName = ''
+      for (const { name, map } of maps) {
+        tName = name
+        for (const [start, end, length] of map) {
+          if (temp >= end && temp < end + length) {
+            temp = temp - end + start
+            if (tName === 'humidity-to-location' && temp < min) min = temp
+            break
+          }
+        }
+      }
+      if (tName === 'humidity-to-location' && temp < min) min = temp
+    }
+  }
+  return min
+}
 console.log(part1(sample))
 console.log(part1(input))
+// console.log(part2(sample))
+// console.log(part1(sample))
+// console.log(part1(input))
+console.log(part2(sample))
+// console.log(part2(input))
+// too high 88151870
+// too high 104847961
+// 12311319
+// 12311318 too high
