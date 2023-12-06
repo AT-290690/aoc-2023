38b93e7e11ef0fc49539f458a66239d9e90a072c
Daniel Rodriguez <maieutiquer@gmail.com>
2023-12-06 02:53:21 +0200

    Daniel: remove printwidth rule for better git diffs

--- 1/Daniel/index.js
+++ 1/Daniel/index.js
@@ -9,12 +9,29 @@ export const solveDay1Part1 = (doc) =>
 
 export const solveDay1Part2 = (
   doc,
-  wordsToDigits = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 },
+  wordsToDigits = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  },
 ) =>
   doc
     .split('\n')
     .map((line) =>
-      [...line.matchAll(new RegExp(`(?=(${Object.keys(wordsToDigits).join('|')}))+|\\d`, 'gi'))]
+      [
+        ...line.matchAll(
+          new RegExp(
+            `(?=(${Object.keys(wordsToDigits).join('|')}))+|\\d`,
+            'gi',
+          ),
+        ),
+      ]
         .flat()
         .filter(Boolean)
         .map((wordOrDigit) => wordsToDigits[wordOrDigit] ?? wordOrDigit),
