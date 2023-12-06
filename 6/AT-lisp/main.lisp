8c7df1b585171195a8c0b830d634a9aa5e6862c5
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-06 23:13:58 +0200

    Separate files

--- 6/AT-lisp/main.lisp
+++ 6/AT-lisp/main.lisp
@@ -1,16 +1,14 @@
 ; To run this code go follow this link
-; https://at-290690.github.io/fez/index.html?l=NwAgCgTgpgzjIGMC2ATEBqE8D2FEBsBLBAaxGwDsQAXACykVsIAdy86BXClKPYy%2BACgAFPijUs2fADcGogIZIARinkhCFZhwnCU2EIJAhR4gFTrqveZeP5FKtdUJJYIFIRjUc244QBmtlAUAOZ0NM6wAJQGRrGxwoSWENZyCCjsETDRwml47p5ZMXHFwsyE4S4whsU1RjnyEJHVtSVI1rQAXDBQAI4cQQhQALQUTS01wg3JAJ4dbawKyqogtFJowubCQ8YIDRVRK2vRq%2FgokefN43VT8rMI2FzUQ9gBiw4g0AjGAHwfUF%2F1PIeLznC5XOIbcjaUFFGoPaigy51RJWGyA9SaaE7PY5dIYrQI6IARlBTRE%2BGwwQAhMYYFJZMYbtNGRAZiAAOwgIkAVhAAGYAAzZJkgACcIAALAKQAAmAVC0FAA
+; https://at-290690.github.io/fez/index.html?l=NwAgCgTgpgzjIGMC2ATEBqE8D2FEBsBLBAaxGwDsQAXACykVsIAdy86BXClKPYy%2BACgAFPijUs2fADcGogIZIARinkhCFZhwnDmhEMITy%2Bm7QEoQgkNZu279hweMR5ATwBcALxYGjeQygmWtRmoVaOEZFOEC4eSPKsCsqq6qY6KNiWUdk51qLiNIRIcn6pwWG5lRH5EiiEMDqlAUHmFVXttrr61EVQ4R0DNsLxdO4wUACOHFAUCFAAtBRm%2FYMDws5u7vGJ%2BIoqarRSaMIAVAbzhcUgh%2FgoFjd3batrGx4I2FzU89gAZga7yTU0AQBgAfCBgSA6g1QrCns9OiNaO5mBBsCgOAgQrCRPhsABzACEBhgUlk0ViFLcIAA7CAAIwAVhAAGYAAwWdYxakAThAABY2SAAExsjk4oA%3D%3D
 
-(let solve (lambda input (do 
-  (let* iterate (lambda times dists out (if (length times) 
-        (iterate (cdr times) (cdr dists) 
-          (pi times
-              (car)
-              (math:sequence-n)
-              (array:map (lambda hold (* (- (car times) hold) hold)))
-              (array:count-of (lambda rec (> rec (car dists))))
-              (* out))) 
-        out)))
-      (iterate (car input) (car (cdr input)) 1))))
-
+(let solve (lambda input (pi (car input)
+                             (array:zip (car (cdr input)))
+                             (array:map (lambda input (do
+                                      (let time (car input))
+                                      (let dist (car (cdr input)))
+                                      (pi time
+                                          (math:sequence-n)
+                                          (array:map (lambda hold (* (- time hold) hold)))
+                                          (array:count-of (lambda rec (> rec dist)))))))
+                             (math:product))))
 (log! (solve (array (array 7 15 30) (array 9 40 200))))
