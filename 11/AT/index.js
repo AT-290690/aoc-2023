685f6478a1f58534a41aa54b12584822d62c3284
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-11 18:12:01 +0200

    Refactor

--- 11/AT/index.js
+++ 11/AT/index.js
@@ -34,62 +34,70 @@ const manhattan = (y1, y2, x1, x2) => Math.abs(x1 - x2) + Math.abs(y1 - y2)
 const distance = (source, target) => {
   return manhattan(source.y, target.y, source.x, target.x)
 }
-const show = (galaxy) =>
-  galaxy.map((x) => x.map((y) => (y ? y.toString() : '.')).join('')).join('\n')
+const show = (universe) =>
+  universe
+    .map((x) => x.map((y) => (y ? y.toString() : '.')).join(''))
+    .join('\n')
 const pairsN = (n) => (n * (n - 1)) / 2
-const part1 = (input) => {
+const pairwise = (galaxies) => {
+  if (galaxies.length < 2) return []
+  const first = galaxies[0],
+    rest = galaxies.slice(1),
+    pairs = rest.map((x) => [first, x])
+  return pairs.concat(pairwise(rest))
+}
+const inputToUniverse = (input) => {
   let count = 0
-  const galaxy = input.map((x) => x.map((y) => (y === '#' ? ++count : 0)))
+  return input.map((x) => x.map((y) => (y === '#' ? ++count : 0)))
+}
+const expandUniverse = (universe) => {
   const horizontal = []
   const vertical = []
-  const height = galaxy.length
-  const width = galaxy[0].length
+  const height = universe.length
+  const width = universe[0].length
   for (let y = 0; y < height; ++y) {
     let h = 0
     let v = 0
     for (let x = 0; x < width; ++x) {
-      if (galaxy[y][x]) h = 1
-      if (galaxy[x][y]) v = 1
+      if (universe[y][x]) h = 1
+      if (universe[x][y]) v = 1
     }
     horizontal.push((1 - h) * 1)
     vertical.push((1 - v) * 1)
   }
-  let adjusted = galaxy
-  adjusted = []
+  const expanded = []
   let max = -Infinity
   for (let y = 0; y < height; ++y) {
-    adjusted.push([])
+    expanded.push([])
     for (let x = 0; x < width; ++x) {
-      if (vertical[x]) adjusted.at(-1).push(0)
-      adjusted.at(-1).push(galaxy[y][x])
+      if (vertical[x]) expanded.at(-1).push(0)
+      expanded.at(-1).push(universe[y][x])
     }
-    if (horizontal[y]) adjusted.push([])
-    max = Math.max(max, adjusted.at(-1).length)
+    if (horizontal[y]) expanded.push([])
+    max = Math.max(max, expanded.at(-1).length)
   }
-  for (let y = 0; y < adjusted.length; ++y) {
-    for (let x = 0; x < adjusted[0].length; ++x) {
-      if (adjusted[y].length === 0)
-        adjusted[y] = Array.from({ length: max }).fill(0)
+  for (let y = 0; y < expanded.length; ++y) {
+    for (let x = 0; x < expanded[0].length; ++x) {
+      if (expanded[y].length === 0)
+        expanded[y] = Array.from({ length: max }).fill(0)
     }
   }
-  const pairwise = (list) => {
-    if (list.length < 2) return []
-    const first = list[0],
-      rest = list.slice(1),
-      pairs = rest.map((x) => [first, x])
-    return pairs.concat(pairwise(rest))
-  }
+  return expanded
+}
+const universeToGalaxies = (universe) => {
   const pairs = []
-  for (let y = 0; y < adjusted.length; ++y) {
-    for (let x = 0; x < adjusted[0].length; ++x) {
-      const v = adjusted[y][x]
+  for (let y = 0; y < universe.length; ++y) {
+    for (let x = 0; x < universe[0].length; ++x) {
+      const v = universe[y][x]
       if (v) pairs.push({ v, x, y })
     }
   }
-  return pairwise(pairs)
+  return pairs
+}
+const part1 = (input) =>
+  pairwise(universeToGalaxies(expandUniverse(inputToUniverse(input))))
     .map(([a, b]) => distance(a, b))
     .reduce((a, b) => a + b, 0)
-}
 const input = parse(read())
 console.log(part1(sample))
 console.log(part1(input))
