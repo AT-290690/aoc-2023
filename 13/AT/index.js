e3dabb45b251c3b05da0112325f6267798de8dc2
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-13 21:24:28 +0200

    Add Day 13 part 1

---
+++ 13/AT/index.js
@@ -0,0 +1,115 @@
+import { read } from '../../AT/utils.js'
+
+const parse = (input) =>
+  input
+    .trim()
+    .split('\n\n')
+    .map(
+      (x) => x.split('\n').map((x) => x.split('').map((x) => +(x === '#')))
+      // .join('\n')
+    )
+// .join('\n\n')
+const sample = parse(`
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`)
+const sample2 = parse(`
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+`)
+
+const sample3 = parse(`
+..##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`)
+Array.prototype.adjacentDifference = function (callback = (a, b) => b - a) {
+  const len = this.length
+  if (len === 1) return this
+  const result = [this[0]]
+  for (let i = 1; i < len; ++i)
+    result[i] = callback(this[i - 1], this[i], i, this)
+  return result
+}
+
+Array.prototype.rotateRight = function () {
+  const result = []
+  this.forEach((a, i, aa) => {
+    a.forEach((b, j, bb) => {
+      result[bb.length - j - 1] = result[bb.length - j - 1] || []
+      result[bb.length - j - 1][i] = b
+    })
+  })
+  return result
+}
+
+Array.prototype.rotateLeft = function (array) {
+  const result = []
+  array.forEach((a, i, aa) => {
+    a.forEach((b, j, bb) => {
+      result[j] = result[j] || []
+      result[j][aa.length - i - 1] = b
+    })
+  })
+  return result
+}
+
+const part1 = (mirrors) => {
+  const rec = (offset, count, arr, out) => {
+    const left = offset - count
+    const right = offset + count + 1
+    if (arr[left] && arr[right]) {
+      out.push(+(arr[left] === arr[right]))
+      return rec(offset, count + 1, arr, out)
+    } else return out
+  }
+
+  const traverse = (mirror) => {
+    const parsed = mirror.map((x) => parseInt(x.join(''), 2))
+    const idxs = []
+    for (let i = 0; i < parsed.length; ++i)
+      idxs.push([i, rec(i, 0, parsed, [])])
+    const [idx, arr] = idxs.find((x) => !x[1].some((x) => x === 0))
+    if (arr.length === 0) return []
+    return [idx + 1, idx + 2]
+  }
+
+  const incidence = []
+  for (const mirror of mirrors) {
+    const hor = traverse(mirror)
+    const ver = traverse(mirror.rotateRight().rotateRight().rotateRight())
+    incidence.push(hor.length > ver.length ? hor.at(0) * 100 : ver.at(0))
+  }
+  return incidence.reduce((a, b) => a + b, 0)
+}
+const input = parse(read())
+console.log(part1(input))
