5621f3cd453c37b8c4d4daa6fbbd0ac32fe559d3
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-14 16:25:06 +0200

    Add solution for Day 14 part 2

--- 13/AT/index.js
+++ 13/AT/index.js
@@ -21,6 +21,34 @@ const sample = parse(`
 #####.##.
 ..##..###
 #....#..#`)
+
+const sample2 = parse(`
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+`)
+
+const sample3 = parse(`
+..##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#...##..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`)
 Array.prototype.adjacentDifference = function (callback = (a, b) => b - a) {
   const len = this.length
   if (len === 1) return this
@@ -80,6 +108,44 @@ const part1 = (mirrors) => {
   }
   return incidence.reduce((a, b) => a + b, 0)
 }
+
+const part2 = (mirrors) => {
+  const rec = (offset, count, arr, out) => {
+    const left = offset - count
+    const right = offset + count + 1
+    if (arr[left] && arr[right]) {
+      out.push(+(arr[left] === arr[right]))
+      return rec(offset, count + 1, arr, out)
+    } else {
+      return out
+    }
+  }
+  const bit = (number, n) => number ^ (1 << n)
+  const traverse = (mirror) => {
+    const parsed = mirror.map((x) => parseInt(x.join(''), 2))
+    const out = []
+    for (let j = 0; j < parsed.length; ++j) {
+      const idxs = []
+      for (let i = 0; i < parsed.length; ++i) {
+        const copy = [...parsed]
+        copy[i] = bit(parsed[i], j)
+        idxs.push([i, rec(i, 0, copy, [])])
+      }
+      const [idx, arr] = idxs.find((x) => !x[1].some((x) => x === 0))
+      out.push(arr.length === 0 ? [] : [idx + 1, idx + 2])
+    }
+    return out.filter((x) => x.length).flat(1)
+  }
+  const incidence = []
+  for (const mirror of mirrors) {
+    const hor = traverse(mirror)
+    const ver = traverse(mirror.rotateRight().rotateRight().rotateRight())
+    console.log({ hor, ver })
+    incidence.push(hor.length > ver.length ? hor.at(0) * 100 : ver.at(0))
+  }
+  return incidence.reduce((a, b) => a + b, 0)
+}
 const input = parse(read())
 console.log(part1(sample))
 console.log(part1(input))
+// console.log(part2(sample))
---
+++ 14/AT/index.js
@@ -0,0 +1,150 @@
+import { read } from '../../AT/utils.js'
+
+const parse = (input) =>
+  input
+    .trim()
+    .split('\n')
+    .map((x) => [...x])
+const sample = parse(`
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`)
+const part1 = (input) => {
+  const matrix = [...input].map((col) => [...col])
+  const H = matrix.length
+  const W = matrix[0].length
+  const shift = (_y, x, matrix) => {
+    for (let y = _y; y < H; ++y) {
+      if (matrix[y][x] === '#') break
+      else if (matrix[y][x] === 'O') {
+        matrix[y][x] = '.'
+        return 'O'
+      }
+    }
+  }
+  for (let y = 0; y < H; ++y) {
+    for (let x = 0; x < W; ++x) {
+      const current = matrix[y][x]
+      if (current === 'O' || current === '#') continue
+      else matrix[y][x] = shift(y, x, matrix) ?? current
+    }
+  }
+  // return matrix.map((x) => x.join('')).join('\n')
+  return matrix.reduce(
+    (a, x, i) => a + x.reduce((a, b) => a + (b === 'O' ? H - i : 0), 0),
+    0
+  )
+}
+const part2 = (input) => {
+  const print = (matrix) =>
+    console.log(matrix.map((x) => x.join('')).join('\n') + '\n')
+  const matrix = [...input].map((col) => [...col])
+
+  const H = matrix.length
+  const W = matrix[0].length
+  const memo = new Map()
+  let count = 0
+  const cycle = () => {
+    // print(matrix)
+    // North
+    const key = matrix.map((x) => x.join('')).join('|')
+    // if (memo.get(key) === 10) return [count, memo]
+    ++count
+    if (memo.has(key)) {
+      memo.set(key, memo.get(key) + 1)
+    } else memo.set(key, 0)
+    for (let y = 0; y < H; ++y) {
+      for (let x = 0; x < W; ++x) {
+        const current = matrix[y][x]
+        if (current === 'O' || current === '#') continue
+        for (let i = y; i < H; ++i) {
+          if (matrix[i][x] === '#') break
+          else if (matrix[i][x] === 'O') {
+            matrix[i][x] = '.'
+            matrix[y][x] = 'O'
+            break
+          }
+        }
+      }
+    }
+    // print(matrix)
+    // West
+    for (let y = 0; y < H; ++y) {
+      for (let x = 0; x < W; ++x) {
+        const current = matrix[y][x]
+        if (current === 'O' || current === '#') continue
+        for (let i = x; i < W; ++i) {
+          if (matrix[y][i] === '#') break
+          else if (matrix[y][i] === 'O') {
+            matrix[y][i] = '.'
+            matrix[y][x] = 'O'
+            break
+          }
+        }
+      }
+    }
+    // print(matrix)
+    // South
+    for (let y = H - 1; y >= 0; --y) {
+      for (let x = 0; x < W; ++x) {
+        const current = matrix[y][x]
+        if (current === '.' || current === '#') continue
+        for (let i = y + 1; i < H; ++i) {
+          if (matrix[i][x] === 'O' || matrix[i][x] === '#') {
+            matrix[y][x] = '.'
+            matrix[i - 1][x] = 'O'
+            break
+          } else if (i === H - 1) {
+            matrix[y][x] = '.'
+            matrix[i][x] = 'O'
+            break
+          }
+        }
+      }
+    }
+    // print(matrix)
+    // East
+    for (let y = 0; y < H; ++y) {
+      for (let x = W - 1; x >= 0; --x) {
+        const current = matrix[y][x]
+        if (current === '.' || current === '#') continue
+        for (let i = x + 1; i <= W; ++i) {
+          if (matrix[y][i] === 'O' || matrix[y][i] === '#') {
+            matrix[y][x] = '.'
+            matrix[y][i - 1] = 'O'
+            break
+          } else if (i === W - 1) {
+            matrix[y][x] = '.'
+            matrix[y][i] = 'O'
+            break
+          }
+        }
+      }
+    }
+    return memo
+  }
+  let sum = 0
+  while (sum !== 100064 && sum !== 64) {
+    cycle()
+    sum = matrix.reduce(
+      (a, x, i) => a + x.reduce((a, b) => a + (b === 'O' ? H - i : 0), 0),
+      0
+    )
+  }
+  return sum
+}
+
+const input = parse(read())
+// console.log(part1(sample))
+// console.log(part1(input))
+console.log(part2(sample))
+console.log(part2(input))
+// 100071 too high
+// 100043 too low
