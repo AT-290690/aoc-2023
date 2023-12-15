e30f2ca842a5cc12130b728f6360b7e916bcaa9d
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-15 11:23:15 +0200

    Add part 2 of Day 15

--- 15/AT/index.js
+++ 15/AT/index.js
@@ -20,6 +20,65 @@ const part1 = (input) => {
     return a + temp
   }, 0)
 }
+const part2 = (input) => {
+  // Holiday ASCII String Helper algorithm
+
+  // Determine the ASCII code for the current character of the string.
+  // Increase the current value by the ASCII code you just determined.
+  // Set the current value to itself multiplied by 17.
+  // Set the current value to the remainder of dividing itself by 256.
+  const hash = (b) => {
+    let temp = 0
+    for (const c of b) {
+      const first = c.charCodeAt(0)
+      temp += first
+      temp *= 17
+      temp %= 256
+    }
+    return temp
+  }
+  // HASHMAP
+  // (Holiday ASCII String Helper Manual Arrangement Procedure)
+  return input
+    .map((x) => {
+      if (x.includes('=')) {
+        const [label, n] = x.split('=')
+        return [label, '=', +n]
+      } else {
+        const [label, n] = x.split('-')
+        return [label, '-', +n]
+      }
+    })
+    .reduce(
+      (box, [label, op, n]) => {
+        const key = hash(label)
+        const current = box[key]
+        const index = current.findIndex(([x]) => x === label)
+        switch (op) {
+          case '-':
+            if (index !== -1) {
+              current[index] = undefined
+              box[key] = box[key].filter(Boolean)
+            }
+            break
+          case '=':
+            if (index !== -1) current[index][1] = n
+            else current.push([label, n])
+            break
+        }
+        return box
+      },
+      Array.from({ length: 256 })
+        .fill(null)
+        .map(() => [])
+    )
+    .reduce(
+      (a, b, i) => a + b.reduce((a, [_, b], j) => a + (i + 1) * b * (j + 1), 0),
+      0
+    )
+}
 const input = parse(read())
 console.log(part1(sample))
 console.log(part1(input))
+console.log(part2(sample))
+console.log(part2(input))
