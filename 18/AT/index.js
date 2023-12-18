15205ae50d34556be4a39a05ecd6073f3d014acc
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-18 20:40:25 +0200

    Clean up

--- 18/AT/index.js
+++ 18/AT/index.js
@@ -37,12 +37,6 @@ U 3 (#a77fa3)
 L 2 (#015232)
 U 2 (#7a21e3)`
 const part1 = (input) => {
-  // const bounds = {
-  //   top: Infinity,
-  //   bottom: -Infinity,
-  //   left: Infinity,
-  //   right: -Infinity,
-  // }
   let cursor = { y: 0, x: 0 }
   const points = []
   let edge = 0
@@ -64,18 +58,8 @@ const part1 = (input) => {
         cursor.y += steps
         break
     }
-
-    // bounds.top = Math.min(bounds.top, cursor.y)
-    // bounds.bottom = Math.max(bounds.bottom, cursor.y)xxxxxxxxx
-    // bounds.left = Math.min(bounds.left, cursor.x)
-    // bounds.right = Math.max(bounds.right, cursor.x)
   }
-  // bounds.bottom = Math.abs(bounds.top) + Math.abs(bounds.bottom)
-  // bounds.top = Math.abs(bounds.top)
-  // bounds.right = Math.abs(bounds.left) + Math.abs(bounds.right)
-  // bounds.left = Math.abs(bounds.left)
   let acc = 0
-  // const points = [...matrix.values()]
   for (let i = 1; i < points.length; ++i) {
     const [y1, x1] = points[i - 1]
     const [y2, x2] = points[i]
