0751e662c813ae7ed427ec793c81cf4ffc23a5b1
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2023-12-18 20:37:37 +0200

    Add AT Day 18 part 2

--- 18/AT/index.js
+++ 18/AT/index.js
@@ -1,5 +1,5 @@
 import { read } from '../../AT/utils.js'
-const parse = (input) =>
+const parse1 = (input) =>
   input
     .trim()
     .split('\n')
@@ -7,7 +7,21 @@ const parse = (input) =>
       const [dir, n, color] = x.split(' ')
       return [dir, +n, color.replace('(', '').replace(')', '').replace('#', '')]
     })
-const sample = parse(`
+const parse2 = (input) =>
+  input
+    .trim()
+    .split('\n')
+    .map((x) => {
+      const [_, _1, color] = x.split(' ')
+      const parsed = color.replace('(', '').replace(')', '').replace('#', '')
+      const last = +parsed[parsed.length - 1]
+      // 0 means R, 1 means D, 2 means L, and 3 means U
+      const map = ['R', 'D', 'L', 'U']
+      const n = parseInt(parsed.slice(0, -1), 16)
+      const dir = map[last]
+      return [dir, n]
+    })
+const sample = `
 R 6 (#70c710)
 D 5 (#0dc571)
 L 2 (#5713f0)
@@ -21,7 +35,7 @@ U 2 (#caa171)
 R 2 (#7807d2)
 U 3 (#a77fa3)
 L 2 (#015232)
-U 2 (#7a21e3)`)
+U 2 (#7a21e3)`
 const part1 = (input) => {
   // const bounds = {
   //   top: Infinity,
@@ -29,34 +43,30 @@ const part1 = (input) => {
   //   left: Infinity,
   //   right: -Infinity,
   // }
-  const matrix = new Map()
   let cursor = { y: 0, x: 0 }
-  const toKey = (y, x) => `${y}|${x}`
+  const points = []
+  let edge = 0
   for (const [direction, steps] of input) {
+    const point = [cursor.y, cursor.x]
+    points.push(point)
+    edge += steps
     switch (direction) {
       case 'R':
-        for (let i = cursor.x; i < cursor.x + steps; ++i)
-          matrix.set(toKey(cursor.y, i), [cursor.y, i])
         cursor.x += steps
         break
       case 'L':
-        for (let i = cursor.x; i >= cursor.x - steps; --i)
-          matrix.set(toKey(cursor.y, i), [cursor.y, i])
         cursor.x -= steps
         break
       case 'U':
-        for (let i = cursor.y; i >= cursor.y - steps; --i)
-          matrix.set(toKey(i, cursor.x), [i, cursor.x])
         cursor.y -= steps
         break
       case 'D':
-        for (let i = cursor.y; i < cursor.y + steps; ++i)
-          matrix.set(toKey(i, cursor.x), [i, cursor.x])
         cursor.y += steps
         break
     }
+
     // bounds.top = Math.min(bounds.top, cursor.y)
-    // bounds.bottom = Math.max(bounds.bottom, cursor.y)
+    // bounds.bottom = Math.max(bounds.bottom, cursor.y)xxxxxxxxx
     // bounds.left = Math.min(bounds.left, cursor.x)
     // bounds.right = Math.max(bounds.right, cursor.x)
   }
@@ -64,16 +74,18 @@ const part1 = (input) => {
   // bounds.top = Math.abs(bounds.top)
   // bounds.right = Math.abs(bounds.left) + Math.abs(bounds.right)
   // bounds.left = Math.abs(bounds.left)
-  const points = [...matrix.values()]
   let acc = 0
+  // const points = [...matrix.values()]
   for (let i = 1; i < points.length; ++i) {
     const [y1, x1] = points[i - 1]
     const [y2, x2] = points[i]
     acc += x1 * y2 - x2 * y1
   }
-  return (acc + points.length) / 2 + 1
+  return (acc + edge) / 2 + 1
 }
-
-const input = parse(read())
-console.log(part1(sample))
-console.log(part1(input))
+const part2 = part1
+const input = read()
+console.log(part1(parse1(sample)))
+console.log(part1(parse1(input)))
+console.log(part2(parse2(sample)))
+console.log(part2(parse2(input)))
